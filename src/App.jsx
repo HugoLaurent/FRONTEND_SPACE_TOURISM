997ab3a06e179d2615f055c5669d239639c7e7bd
HugoLaurent <lauhugopro@gmail.com>
2024-10-12 16:51:20 +0200

    Update App.jsx to include Navbar component and pass language data to Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Home } from "./Pages"; // Assurez-vous d'importer votre composant Home
+import { Home, Navbar } from "./Pages"; // Assurez-vous d'importer votre composant Home
 import languageDatas from "./datas/language.json"; // Importer le JSON contenant les langues
 
 export default function App() {
@@ -9,26 +9,11 @@ export default function App() {
   // Récupération des données de la langue sélectionnée
   const languageToUse = languageDatas[language] || languageDatas["eng"];
 
-  // Fonction pour changer de langue
-  const changeLanguage = (langCode) => {
-    setLanguage(langCode);
-  };
-
   return (
     <Router>
-      <nav>
-        <ul>
-          {languageToUse.navbar.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-        {/* Boutons pour changer de langue */}
-        <button onClick={() => changeLanguage("fr")}>FR</button>
-        <button onClick={() => changeLanguage("eng")}>EN</button>
-      </nav>
-
+      <Navbar language={languageToUse.navbar} setLanguage={setLanguage} />
       <Routes>
-        <Route path="/" element={<Home language={languageToUse.home} />} />
+        <Route path="/" element={<Home language={languageToUse} />} />
       </Routes>
     </Router>
   );
