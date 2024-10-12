a80ecb549b75e0fd61afd3bd8cfbf6246d0f4898
HugoLaurent <lauhugopro@gmail.com>
2024-10-12 17:47:58 +0200

    Update Home component to include language data and improve styling

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,19 @@
 import "./home-style.css";
 
-export default function Home() {
-  return <div className="home">Coucou</div>;
+export default function Home({ language }) {
+  return (
+    <div className="home">
+      <div className="home__wrapper">
+        <div className="home__text">
+          <p className="home__text--subtitle">{language.home.subtitle}</p>
+          <h1 className="home__text--title">{language.home.title}</h1>
+          <p className="home__text--content">{language.home.content}</p>
+        </div>
+        <button className="home__button">
+          <span className="home__button--p">{language.home.button}</span>
+          <div className="moon"></div>
+        </button>
+      </div>
+    </div>
+  );
 }
